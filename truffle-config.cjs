79705af3f58b30620a68612c5d914c5bffb0f18b
slashexx <dhruvpuri.35@gmail.com>
2024-08-27 19:48:14 +0530

    Removed env file coz BRUH

--- truffle-config.cjs
+++ truffle-config.cjs
@@ -13,14 +13,15 @@ module.exports = {
         process.env.MNEMONIC, 
         'https://zetachain-athens-evm.blockpi.network/v1/rpc/public'
       ),
-      network_id: 7001,
-      gas: 4500000,
-      gasPrice: 10000000000,
+      network_id: 7001, // ZetaChain network ID
+      gas: 4500000, // Gas limit
+      gasPrice: 10000000000, // Gas price in wei
+      networkCheckTimeout: 100000, // Network check timeout in ms
     },
   },
   compilers: {
     solc: {
-      version: "0.8.0"
+      version: "0.8.0" // Specify the Solidity compiler version
     }
   }
 };
